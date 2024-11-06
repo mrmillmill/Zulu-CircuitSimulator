b760fec090a26dda69dbec985dea4f6ead36ca5c
mrmillmill <mrmillmill@gmail.com>
2024-11-05 17:57:55 -0800

    combined front end and backend basic operations

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    // Test API call to the backend
+    fetch('/api/test')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple Circuit Simulator</h1>
+      <p>Welcome to the Circuit Simulator!</p>
     </div>
   );
 }
